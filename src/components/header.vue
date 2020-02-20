505302c633ae04828c427fc2a691116d6a326101
cotwelf <812811926@qq.com>
2020-02-20 21:29:09 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <mu-appbar :color="this.$store.state.global.theme.color" style="height:100px;padding-top:10%">
+    <mu-appbar :color="this.$store.state.global.theme.color" class="header">
       {{title}}
       <!-- <mu-button flat slot="right">
         <i class="iconfont icon-shezhi" style="font-size: 30px"></i>
@@ -9,6 +9,7 @@
   </div>
 </template>
 <script>
+import css from '@/assets/css/compatible.css'
 export default {
   props: ["title"],
   data() {
